51cae547374783b51e0d4a82c3fd17d191805344
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-08-04 04:15:37 +0200

    Ready for deployment

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: require('node:path').join(__dirname, '../../'),
+  },
+};
 
 export default nextConfig;
