c7d207f4c983623f1802e3ca2582ea69ddcb811a
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-08-20 13:44:11 +0200

    Added posthog

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,24 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   outputFileTracingRoot: require('node:path').join(__dirname, '../../'),
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/flags',
+        destination: 'https://eu.i.posthog.com/flags',
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
